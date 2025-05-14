dc84dbbe86297fabaf581865e1120526150b79f5
sunmeng <sunmeng@163.com>
2025-05-14 16:09:05 +0800

    fix:通过 mongoose连接 mongodb

--- index.md
+++ index.md
@@ -1,6 +1,5 @@
 docker ps
 
-docker exec -it lego-mongodb mongo
 // 进入容器
 docker exec -it lego-mongodb bash
 
@@ -8,10 +7,28 @@ docker exec -it lego-mongodb bash
 mongosh --host localhost --port 27017
 
 db.auth('admin', 'admin1234')
-
+db.auth('schoolAdmin', 'admin1234')
 mongodb 天然支持分布式
 
 客户端并发量过大
 
 1. 带宽不够
 2. 数据量太大
+   [
+   {
+   \_id: 'admin.admin',
+   userId: UUID('7991bf43-7a0e-4b11-85f5-28257a74354f'),
+   user: 'admin',
+   db: 'admin',
+   roles: [ { role: 'root', db: 'admin' } ],
+   mechanisms: [ 'SCRAM-SHA-1', 'SCRAM-SHA-256' ]
+   },
+   {
+   \_id: 'admin.schoolAdmin',
+   userId: UUID('5ea7b937-3a3f-4ffa-8f0c-aec7b26a558b'),
+   user: 'schoolAdmin',
+   db: 'admin',
+   roles: [ { role: 'readWrite', db: 'school' } ],
+   mechanisms: [ 'SCRAM-SHA-1', 'SCRAM-SHA-256' ]
+   }
+   ]
--- src/index.js
+++ src/index.js
@@ -1,11 +1,37 @@
 let mongoose = require("mongoose");
 /**
  * mongodb:协议 http://localhost:27017/
+ * schoolAdmin:账号
+ * admin1234:密码
+ * school:数据库
  * */
-let connection = mongoose.createConnection("mongodb://localhost:27017/school");
-connection.on("error", function (error) {
-  console.log("数据库连接失败: " + error);
+let connection = mongoose.createConnection(
+  "mongodb://schoolAdmin:admin1234@localhost:27017/school"
+);
+
+// 定义一个schema,g规定了一个集合中文档的属性名和属性的类型
+//  一个Schmea对应一个集
+
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  password: String,
+  createAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
-connection.on("open", function (error) {
-  console.log("数据库连接成功");
-});
+
+// 定义数据库的操作模型
+let User = connection.model("User", UserSchema);
+
+async function createUser() {
+  try {
+    //往集合插入数据
+    const result = await User.create({ username: "张三", age: 10 });
+    console.log(result);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+createUser();
